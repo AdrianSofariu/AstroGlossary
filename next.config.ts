f3b26303cf036a6dd80f84c04f68967890fc3cee
AdrianSofariu <adrianradusofariu@gmail.com>
2025-05-14 12:38:53 +0300

    prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    API_CONNECTION_STRING: "http://172.30.244.168:3001/api",
-    API_SOCKET: "http://172.30.244.168:3001",
+    API_CONNECTION_STRING: process.env.API_CONNECTION_STRING,
+    API_SOCKET: process.env.API_SOCKET,
   },
   images: {
     remotePatterns: [
@@ -26,23 +26,9 @@ const nextConfig: NextConfig = {
           "/wikipedia/commons/c/ca/Star-forming_region_S106_%28captured_by_the_Hubble_Space_Telescope%29.jpg",
       },
       {
-        protocol: "http", // Use 'http' for local development server (Next.js uses http locally by default)
-        hostname: "192.168.0.14",
-        pathname: "/images/*", // Allow any image in the /images/ folder
-      },
-      {
-        protocol: "http", // Use 'http' for local development server (Next.js uses http locally by default)
-        hostname: "172.30.244.167",
-        pathname: "/images/*", // Allow any image in the /images/ folder
-      },
-      {
         protocol: "http", // Allow any subdomain of localhost
-        hostname: "*.localhost",
+        hostname: "api.com",
         pathname: "/images/*", // Allow any image in the /images/ folder
-      },
-      {
-        protocol: "https",
-        hostname: "*",
       },
     ],
   },
